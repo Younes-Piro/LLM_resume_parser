64091374ce4aec9453b9be6c6f77e1eafb83ce59
Younes-Piro <piroyounes148@gmail.com>
2023-11-15 16:04:34 +0100

    change the pdf reader & modifie conversation for better result

--- cv_parser.py
+++ cv_parser.py
@@ -1,7 +1,12 @@
 import openai
 from dotenv import load_dotenv, find_dotenv
 import os
-from langchain.document_loaders import PyPDFLoader
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+import io
+import json
 
 _ = load_dotenv(find_dotenv()) # read local .env file
 
@@ -9,34 +14,33 @@ openai.api_key  = os.environ['OPENAI_API_KEY']
 
 def read_cv(cv_path):
     # load cv file
-    loader = PyPDFLoader(cv_path)
-    documents = loader.load()
+    i_f = open(cv_path,'rb')
+    resMgr = PDFResourceManager()
+    retData = io.StringIO()
+    TxtConverter = TextConverter(resMgr,retData, laparams= LAParams())
+    interpreter = PDFPageInterpreter(resMgr,TxtConverter)
+    for page in PDFPage.get_pages(i_f):
+        interpreter.process_page(page)
+ 
+    txt = retData.getvalue()
+    return txt
 
-    result = ""
-    if len(documents) > 1:
-        for doc in documents:
-           result = f"{result} {doc.page_content}" 
-    else:
-        result = f"{result} {documents[0].page_content}"
 
-    return result
-
-
-def parse_cv(cv_text, openai_api_key):
+def parse_cv(cv_text, openai_api_key, max_tokens=500):
     openai.api_key = openai_api_key
 
     # Define the conversation with the CV text as a variable
     conversation = [
-        {"role": "system", "content": "You are a CV parsing assistant."},
-        {"role": "user", "content": f"Parse the following CV and extract relevant information:\n\n{cv_text}"},
-        {"role": "assistant", "content": "---\nExtracted Information:\n1. Personal Information:\n   - Full Name:\n   - Contact Information:\n\n2. Education:\n   - Degree:\n   - Major:\n   - School/University:\n   - Graduation Year:\n\n3. Work Experience:\n   - Position:\n   - Company:\n   - Duration:\n   - Responsibilities/Accomplishments:\n\n   - Position:\n   - Company:\n   - Duration:\n   - Responsibilities/Accomplishments:\n\n4. Skills:\n   - List of skills mentioned in the CV.\n\n5. Additional Information:\n   - Any additional noteworthy details.\n\n---\nClarifications/Questions:\n- If any information is unclear or missing, ask for clarification or request additional details."}
+        {"role": "system", "content": "Vous êtes un assistant d'analyse de CV."},
+        {"role": "user", "content": f"Analysez le CV suivant et extrayez les informations pertinentes :\n\n{cv_text}"},
+        {"role": "assistant", "content": "Informations extraites en JSON avec exactement la structure suivante : {information_basique : {nom, prenom, email, numero_telephone, location, linkedin_url, nom_universite, niveau_education, titre}, Expérience_professionnelle : [{Poste, Entreprise, Durée, Responsabilités/Réalisations}]}"},
     ]
 
     # Make the API call
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=conversation,
-    max_tokens=400
+    max_tokens=max_tokens
     )
 
     # Extract the assistant's reply
@@ -45,7 +49,9 @@ def parse_cv(cv_text, openai_api_key):
     # Print or use the assistant's reply as needed
     return assistant_reply
 
-
+def load_json(text):
+    result_json=json.loads(text)
+    return result_json
 
 
 
