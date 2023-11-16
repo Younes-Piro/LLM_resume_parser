a06308efeb35b41d2c3ed5b839867143e3dd6e77
Younes-Piro <piroyounes148@gmail.com>
2023-11-16 16:12:11 +0100

    new function with prompt template includes examples cv

--- cv_reader_langchain.py
+++ cv_reader_langchain.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import json
 from dotenv import load_dotenv, find_dotenv
 from langchain.prompts import PromptTemplate
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
@@ -11,7 +12,7 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
-def read_cv(cv):
+def read_cv_output_parser(cv):
 
     # load cv file
     loader = PyPDFLoader(cv)
@@ -71,6 +72,239 @@ def read_cv(cv):
     result = qa_chain({
         "questions": 'list all the candidate informations',
         "context": documents
+    })
+
+    return result["text"]
+
+
+def read_cv(cv):
+
+    # load cv file
+    loader = PyPDFLoader(cv)
+    documents = loader.load()
+
+    # Prompt template with examples
+
+    example_cv = '''PIRO YOUNES
+
+        Étudiant en master Big Data 
+
+        +212 6 94 800 614
+
+        28 Comp AHLAN A2 IM 28, Tangier
+        piroyounes148@gmail.com 
+
+        linkedin.com/in/younes-piro
+
+        github.com/Younes-piro
+
+        Experience 
+
+        Education 
+
+        Data Scientist Intern
+
+        PCA - Payment Center For Africa - Casablanca
+
+        Février 2023 -
+        Juillet 2023
+
+        Création d'un modèle d'apprentissage automatique
+        pour prédire l'appétence des clients envers
+        l'utilisation des canaux digitaux
+
+        Data Scientist Intern
+
+        Juillet 2022 -
+        Septembre 2022
+
+        Full-stack dévelopeur
+        intern
+
+        Mars 2021 - Juin 2021
+
+        CMAIS - Compagnie Méditerranéenne d'Analyse
+
+        et d'Intelligence Stratégique - Rabat
+
+        Assistance au développement d'une solution de
+        knowledge management
+
+        Maroc Advisor - Tanger
+
+        Conception et Création d’une application web sous forme
+        d’une solution pour les rendez-vous,  les paiements
+
+            et facturation pour un cabinet de kinésithérapie 
+
+        2021-2023
+
+        2020-2021
+
+        2017-2020
+
+        
+
+        Faculté des sciences et techniques de Tanger
+        Master en Mobiquité et Big Data 
+
+        Faculté des sciences et techniques de Tanger
+        Licence en Génie informatique 
+
+        Faculté des sciences et techniques de Tanger
+        DEUST, MIPC (mathématique, Informatique , Physique et Chimie) 
+
+        Certification
+
+        IBM  - Big Data Engineer 2021 Mastery Award  Identifiant de la certification 4650-1655-9802-3105
+        IBM - IOT Cloud Developer 2021 Mastery Award  Identifiant de la certification 4665-1658-1469-8442
+        LinkedIn - Machine learning : Traitement du langage naturel avec Python
+        365 Data Science : Power BI certification
+
+        Academic
+        Projects
+
+        2022
+
+        2022
+
+        2022-2023
+
+        Développement  d'une  plateforme  web  d'ETL  et
+        de  visualisation  pour  l'analyse  des  données  des
+        appartements au Maroc
+
+        Technologies et outils: angular13 - Django - pandas - Plotly -
+        BeautifulSoup -  MongoDb - Elasticsearch - kebana - graphql
+
+        Réalisation  d'une  solution  décisionnelle  pour
+        analyser les indicateurs clés de performance (KPI)
+        de 
+        la  Faculté  des  Sciences  et  Techniques  de
+        Tanger
+
+        Technologies et outils: Microsoft Power BI - MS-BI(SSIS - SSAS)
+
+        Application  web  pour  consumer  un  model  deep
+        learning  base  sur  une  architecture  LSTM  pour
+        prédire les ventes futures
+
+        Technologies et outils: React - FASTAPI - RNN - LSTM - TensorFlow -
+        Keras 
+
+        Skills
+
+        Langues
+
+        Programming Languanges
+
+        Business Intelligence:
+
+        DB:
+
+        web development
+
+        ML - DL :
+
+        Python(Pandas, Numpy, Matplotlib,
+        Seaborn)
+        Microsoft Power BI/ MS-BI(SSIS-SSAS)
+
+        SQL Server/ Mysql/ mongodb
+
+        Django - Flask - FastApi 
+
+        NLP - Tenserflow - Keras - Scikit learn
+
+        Arabe
+
+        Francais
+
+        Anglais
+
+        langue maternelle
+
+        Courant
+
+        Technique
+        '''
+
+    result_cv='''
+    {
+    "informations_basiques": {
+        "nom": "PIRO",
+        "prenom": "YOUNES",
+        "email": "piroyounes148@gmail.com",
+        "numero_telephone": "+212 6 94 800 614",
+        "location": "28 Comp AHLAN A2 IM 28, Tangier",
+        "linkedin_url": "linkedin.com/in/younes-piro",
+        "nom_universite": "Faculté des sciences et techniques de Tanger",
+        "niveau_education": "Master en Mobiquité et Big Data",
+        "titre": "Étudiant en master Big Data"
+    },
+    "expérience_professionnelle": [
+        {
+        "poste": "Data Scientist Intern",
+        "entreprise": "PCA - Payment Center For Africa",
+        "durée": "Février 2023 - Juillet 2023",
+        "responsabilités/réalisations": ["Création d'un modèle d'apprentissage automatique pour prédire l'appétence des clients envers l'utilisation des canaux digitaux"]
+        },
+        {
+        "poste": "Data Scientist Intern",
+        "entreprise": "CMAIS - Compagnie Méditerranéenne d'Analyse et d'Intelligence Stratégique",
+        "durée": "Juillet 2022 - Septembre 2022",
+        "responsabilités/réalisations": ["Assistance au développement d'une solution de knowledge management"]
+        },
+        {
+        "poste": "Full-stack dévelopeur intern",
+        "entreprise": "Maroc Advisor",
+        "durée": "Mars 2021 - Juin 2021",
+        "responsabilités/réalisations": ["Conception et Création d’une application web sous forme d’une solution pour les rendez-vous, les paiements et facturation pour un cabinet de kinésithérapie"]
+        }
+    ]
+    }
+    '''
+
+    result_json=json.loads(result_cv)
+
+    assistance_template = """ 
+
+        Voici un exemple qui doit vous aidez pour analyser d'une maniere precise le CV:
+
+        Input: {example_cv} \n\n
+        Output: ```json {result_cv} ```
+
+    """
+
+    assistance_prompt = assistance_template.format(example_cv=example_cv, result_cv=result_json)
+
+    # prompt
+    PROMPT = '''
+        Vous êtes un assistant d'analyse de CV
+        
+        Analysez le CV suivant et extrayez les informations pertinentes :\n\n {cv_text}
+        
+        Informations extraites en JSON avec exactement la structure suivante : "information_basique : [nom, prenom, email, numero_telephone, location, linkedin_url, nom_universite, niveau_education, titre], Expérience_professionnelle : [Poste, Entreprise, Durée, Responsabilités/Réalisations]"
+    '''
+
+    FINAL_PROMPT = PROMPT + assistance_prompt
+
+    # prompt template
+    prompt_template = PromptTemplate(
+        template=FINAL_PROMPT,
+        input_variables=["cv_text"]
+    )
+
+    # load LLM
+    llm_name = 'gpt-3.5-turbo'
+    llm = ChatOpenAI(model_name=llm_name, temperature=0)
+
+    # load chain
+    qa_chain = LLMChain(llm=llm, prompt=prompt_template)
+
+    # list the informations
+    result = qa_chain({
+        "cv_text": documents
     })
 
     return result["text"]
