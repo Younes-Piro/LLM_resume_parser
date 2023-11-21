e19bec4c454c59a5f011bbc237c8ec37731693b6
Younes-Piro <piroyounes148@gmail.com>
2023-11-21 16:05:22 +0100

    adding connection to mongodb

---
+++ db_connection.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
+import os
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+class DB:
+    def __init__(self):
+        self.client = MongoClient(os.environ['CONNECTION_STRING'])
+        self.db = self.client[os.environ['DB']]
+        self.collection = self.db[os.environ['COLLETION']]
+
+    def add_data(self, data):
+        self.collection.insert_one(data)
+
