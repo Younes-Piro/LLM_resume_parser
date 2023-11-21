c29b364a9276eb21495454db141d11f0708e0850
Younes-Piro <piroyounes148@gmail.com>
2023-11-21 15:40:34 +0100

    edit total_experiences function and adding formatting function to push tomongo

--- cv_parser.py
+++ cv_parser.py
@@ -8,6 +8,8 @@ from pdfminer.layout import LAParams
 import io
 import json
 import re
+from dateutil import parser
+from datetime import datetime
 
 
 _ = load_dotenv(find_dotenv()) # read local .env file
@@ -57,23 +59,65 @@ def load_json(text):
     result_json=json.loads(text)
     return result_json
 
+# convert experiences duration to specific format
+def convert_to_standard_format(date_str):
+    # Try to parse the date using dateutil.parser
+    try:
+        parsed_date = parser.parse(date_str)
+    except ValueError:
+        return None
+
+    # Format the parsed date into 'YYYY-MM' format
+    formatted_date = datetime.strftime(parsed_date, "%Y-%m")
+    return formatted_date
+
+#extract and sum all work experiences
 def extract_experience_duration(experiences):
-    work_duration = []
+    all_experience_duration = []
+    num_exp = 0
+    for exp in experiences:
+        if not any(keyword in exp["Poste"].lower() for keyword in ["internship", "intern", "stage", "pfe"]): 
+            # Use the convert_to_standard_format function for start_date and end_date
+            start_date_str = convert_to_standard_format(exp['Durée']['start_date'])
+            end_date_str = convert_to_standard_format(exp['Durée']['end_date']) if exp['Durée']['end_date'].lower() != 'present' else datetime.now().strftime("%Y-%m")
+            
+            # Try to parse the standardized dates
+            start_date = datetime.strptime(start_date_str, "%Y-%m") if start_date_str else None
+            end_date = datetime.strptime(end_date_str, "%Y-%m") if end_date_str else datetime.now()
+            
+            # Calculate the difference in months
+            if start_date:
+                months_difference = (end_date.year - start_date.year) * 12 + end_date.month - start_date.month
+                all_experience_duration.append(months_difference)
+           
+    return ( sum(all_experience_duration) + len(all_experience_duration) - 1 )  
+
+# extract all responsibilites from different experiences
+def all_responsibilies(experiences):
+    all_resp = []
     for exp in experiences:
         if not any(keyword in exp["Poste"].lower() for keyword in ["internship", "intern", "stage", "pfe"]):
-            date = f"{exp['Durée']['start_date']} - {exp['Durée']['end_date']}"
-            work_duration.append(date)
-    return work_duration    
-
-def result_json(result):
-    # Use regular expression to extract the JSON object from the text
-    json_match = re.search(r'\{.*\}', result, re.DOTALL)
+            all_resp.extend(exp['Responsabilités/Réalisations'])
     
-    try:
-        # Parse the extracted JSON
-        extracted_json = json.loads(json_match.group())
-        return extracted_json
-    except json.JSONDecodeError as e:
-        print("Error decoding JSON:", e)
+    return all_resp   
+
+def format_data(cv_parsed):
+  data = {}
+
+  #basic information
+  data['nom'] = cv_parsed['information_basique']['nom']
+  data['prenom'] = cv_parsed['information_basique']['prenom']
+  data['email'] = cv_parsed['information_basique']['email']
+  data['phone'] = cv_parsed['information_basique']['numero_telephone']
+  data['location'] = cv_parsed['information_basique']['location']
+  data['titre'] = cv_parsed['information_basique']['titre_professionnel']
+
+  # numero mois d'experience
+  data['month_experiences'] = extract_experience_duration(cv_parsed['Expérience_professionnelle'])
+
+  # responsibilites
+  data['responsibilities'] = all_responsibilies(cv_parsed['Expérience_professionnelle'])
+
+  return data
 
 
